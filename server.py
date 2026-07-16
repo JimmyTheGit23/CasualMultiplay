1ef7dfc4aae5fb2e61cbcd08383850c2570744a8
crazyjimmy233 <poorjimmy@hotmail.com>
2026-07-16 15:14:57 +0800

    feat: 新增 Dear Passengers (亲爱的乘客)
    - appid=4534960, 开发者 FLEXUS
    - 状态: WISHLIST (2026 年发售)
    - 平台: Steam
    - Steam 官方 tag: Online Co-Op, Multiplayer, Co-op, Comedy, Action, Funny, Adventure, Physics, First-Person, 3D
    - 玩法: 合作扮演机组人员,飞机解体+货物非法+乘客安全
    - 赛道: 合作喜剧物理 (与 Content Warning/PEAK 同赛道)
    - priority: P1
    
    Land or Die 是 Roblox 平台游戏,不在 Steam,故未加入 registry
    
    类似 tag 游戏 (重叠 >=4):
    - 巅峰梦序 (6/8) - PEAK
    - R.E.P.O. (5/8)
    - 青蛙小队 (5/8)
    - Content Warning (4/8)
    - 超级变色龙 (4/8)
    - 绝地潜兵2 (4/8)
    - 地精捣蛋团 (4/8)
    
    DataValidator 18/18 PASS

---
+++ server.py
@@ -0,0 +1,29 @@
+"""No-cache preview server for E:\CasualMultiplay\docs
+Run: python E:\CasualMultiplay\server.py
+Open: http://localhost:8083/
+"""
+import http.server
+import socketserver
+import os
+import sys
+
+PORT = 8083
+ROOT = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'docs')
+
+
+class NoCacheHandler(http.server.SimpleHTTPRequestHandler):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, directory=ROOT, **kwargs)
+
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate, max-age=0')
+        self.send_header('Pragma', 'no-cache')
+        self.send_header('Expires', '0')
+        super().end_headers()
+
+
+if __name__ == '__main__':
+    with socketserver.TCPServer(('127.0.0.1', PORT), NoCacheHandler) as httpd:
+        print(f'Preview server on http://localhost:{PORT}/')
+        print(f'Serving: {ROOT}')
+        httpd.serve_forever()
